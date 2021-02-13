dc9ee14c7058bb97d9021e159eb2cfb3d5337f8c
Joshua Carlson <jmcarlson0320@gmail.com>
2021-02-12 18:22:58 -0800

    identity matrix function

--- main.c
+++ main.c
@@ -20,6 +20,26 @@ float **build_matrix(int n)
 }
 
 
+float **ident_matrix(int n)
+{
+    float **mat = malloc(n * sizeof(float *));
+    for (int i = 0; i < n; i++) {
+        mat[i] = malloc(n * sizeof(float));
+    }
+
+    srand(time(NULL));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j)
+                mat[i][j] = 1.0f;
+            else
+                mat[i][j] = 0.0f;
+        }
+    }
+
+    return mat;
+}
+
 void free_matrix(float **mat, int n)
 {
     for (int i = 0; i < n; i++) {
@@ -41,17 +61,22 @@ void print_matrix(float **mat, int n)
 }
 
 
-void serial_mult(float **a, float **b, int n)
+// c = a * b
+void serial_mult(float **c, float **a, float **b, int n)
 {
+
 }
 
 
 int main(int argc, char *argv[])
 {
     printf("matrix multiplier\n");
+    int size = 3;
+    if (argc == 2)
+        size = atoi(argv[1]);
     float **matrix = NULL;
-    matrix = build_matrix(3);
-    print_matrix(matrix, 3);
-    free_matrix(matrix, 3);
+    matrix = ident_matrix(size);
+    print_matrix(matrix, size);
+    free_matrix(matrix, size);
     return 0;
 }
